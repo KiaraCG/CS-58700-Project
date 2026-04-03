1371b5b4b699d9a2a13245e136742442447d51bb
Kiara Chau Garcia <kchaugarcia@student.ethz.ch>
2026-04-02 22:18:13 -0400

    Correct data path

--- reflection-equivariance/birds_loader.py
+++ reflection-equivariance/birds_loader.py
@@ -5,7 +5,7 @@ import kagglehub
 from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
 
-bird_data_dir = "/scratch/scholar/jchaugar/inatbirds"
+bird_data_dir = "/scratch/scholar/jchaugar/inatbirds/bird_train"
 
 
 def download_data():
